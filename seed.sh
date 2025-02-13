0dd40d2ff082892e692b17803b9d14b1f2a1c25a
raffimolero <raffimolero@gmail.com>
2025-02-13 23:25:29 +0800

    double generate in seed

--- seed.sh
+++ seed.sh
@@ -4,6 +4,7 @@ docker compose down
 docker compose up db -d
 sleep 0.5s
 
+npx prisma generate # for some reason this one gets its permissions denied
 npx prisma generate
 npx prisma migrate deploy
 npx prisma db seed
