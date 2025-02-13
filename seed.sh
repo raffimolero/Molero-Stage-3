ab154b09fd683a985a5df2e750fc9aaa4e4798f5
raffimolero <raffimolero@gmail.com>
2025-02-13 23:29:27 +0800

    notes in seed.sh

--- seed.sh
+++ seed.sh
@@ -4,7 +4,9 @@ docker compose down
 docker compose up db -d
 sleep 0.5s
 
+echo "Note: The first generate command throws an error on my machine. It will execute twice."
 npx prisma generate # for some reason this one gets its permissions denied
+echo "Note: The following operation takes about 7 seconds on my machine. It does not have a loading bar or icon or anything."
 npx prisma generate
 npx prisma migrate deploy
 npx prisma db seed
